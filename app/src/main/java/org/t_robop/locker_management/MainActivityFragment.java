536aa4bf99bc74a3cc2287a17218e85ef02621f4
Kishimoto <kissy0613@gmail.com>
2018-06-26 18:58:15 +0900

    カスタムアダプターを追加

--- app/src/main/java/org/t_robop/locker_management/MainActivityFragment.java
+++ app/src/main/java/org/t_robop/locker_management/MainActivityFragment.java
@@ -6,24 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainActivityFragment extends Fragment {
+        private static final String[] countries = {
+                "一段目","二段目","三段目","四段目","五段目"
+        };
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_main_activity, container, false);
+            ListView listView = (ListView) view.findViewById(R.id.listView);
+            CustomAdapter adapter = new CustomAdapter(view.getContext(), R.layout.row, countries);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-    public MainActivityFragment() {
-        // Required empty public constructor
+                    switch (view.getId()) {
+                        case R.id.edit:
+//
+                            break;
+                        case R.id.delete:
+                            break;
+                    }
+                }
+            });
+            //ここに書く
+            return view;
+        }
+
     }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_activity, container, false);
-    }
-
-}
